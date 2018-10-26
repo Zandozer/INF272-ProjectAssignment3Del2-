8a42af585076149871b8151af04b5bfcec53d883
Zandozer <u15179746@tuks.co.za>
2018-10-26 15:05:44 +0200

    created project

---
+++ INF272-ProjectAssignment3Del02/INF272-ProjectAssignment3Del02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="INF272_ProjectAssignment3Del02.MvcApplication" Language="C#" %>
---
+++ INF272-ProjectAssignment3Del02/INF272-ProjectAssignment3Del02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(INF272_ProjectAssignment3Del02.Startup))]
+namespace INF272_ProjectAssignment3Del02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
